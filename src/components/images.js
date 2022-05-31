891ea69db5f8b7c623bf3f1fa7a90ba2285f54c2
Ilgiz Temi <temirbekov91@gmail.com>
2022-05-31 18:01:39 -0400

    carousel react project

---
+++ src/components/images.js
@@ -0,0 +1,14 @@
+export const Images = [
+    {image: 'https://images.unsplash.com/photo-1598816379790-43364e52c3e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXRhaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'},
+    {image: 'https://images.unsplash.com/photo-1574970324184-746b18c8bd49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXRhaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'},
+    {image: 'https://images.unsplash.com/photo-1434730737257-3e97ad16f4b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dXRhaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60'},
+    {image: 'https://images.unsplash.com/photo-1557939403-1760a0e47505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHV0YWh8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60'},
+    {image: 'https://images.pexels.com/photos/2542329/pexels-photo-2542329.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
+    {image: 'https://images.unsplash.com/photo-1590089927413-005fdb6555dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHV0YWh8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60'},
+    {image: 'https://images.unsplash.com/photo-1575408264798-b50b252663e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1953&q=80'},
+    {image: 'https://images.unsplash.com/photo-1504282706065-f5866e9cbeeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'},
+    {image: 'https://images.unsplash.com/photo-1596910441000-01c1e7f9ce70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80'},
+    {image: 'https://images.unsplash.com/photo-1581689942833-c116692b0bba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},
+    {image: 'https://images.unsplash.com/photo-1582647589973-8dfb4951e36c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},
+    {image: 'https://images.unsplash.com/photo-1595282092548-a1cb2a8fd242?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'}
+]
---
+++ src/components/slider.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Images } from "./images";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
+const Slider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+      {Images.map((img, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img src={img.image} className="img" alt="images of utah" />
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Slider;
